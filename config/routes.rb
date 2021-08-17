e0b88e0968c999f3b0f94a2173fe51e634d75675
Igor Depolli <igorcler@gmail.com>
2021-08-17 16:06:47 -0300

    Day 26 - Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # List all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # New task form
+  get 'tasks/new', to: 'tasks#new'
+
+  # Save a task in DB
+  post 'tasks', to: 'tasks#create'
+
+  # Show one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Edit task form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+
+  # Update task in DB
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
---
+++ test/models/r_r_task_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class RRTaskTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
